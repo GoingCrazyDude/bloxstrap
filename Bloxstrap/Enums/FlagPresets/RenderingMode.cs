7b71a99a4facf85f64dea0e53ab3cc061fffaa10
GoingCrazyDude <tezosbiswa@gmail.com>
2024-09-17 00:06:15 +0530

    Re-added Vulkan

--- Bloxstrap/Enums/FlagPresets/RenderingMode.cs
+++ Bloxstrap/Enums/FlagPresets/RenderingMode.cs
@@ -4,9 +4,9 @@
     {
         [EnumName(FromTranslation = "Common.Automatic")]
         Default,
-        // Vulkan,
+        Vulkan,
         D3D11,
         D3D10,
-        // OpenGL
+        OpenGL
     }
 }
